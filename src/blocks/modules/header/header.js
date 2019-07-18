af21d61888c1160fc3f39a0b57702feb2fb5545f
Andrey Gorokhov <ag.alexeich@gmail.com>
2019-07-18 18:14:26 +0300

    Fixed hamburger animation

--- src/blocks/modules/header/header.js
+++ src/blocks/modules/header/header.js
@@ -4,26 +4,9 @@ const hamburger = document.querySelector(".header__hamburger");
 
 hamburger.onclick = function() {
     menu.style.display = menu.style.display === "flex" ? "none" : "flex";
+    hamburger.classList.toggle("is-active");
     header.classList.toggle("header-show-menu");
 };
-
-(function() {
-    "use strict";
-
-    const toggles = document.querySelectorAll(".header__hamburger");
-
-    for (let i = toggles.length - 1; i >= 0; i--) {
-        let toggle = toggles[i];
-        toggleHandler(toggle);
-    }
-
-    function toggleHandler(toggle) {
-        toggle.addEventListener("click", function(e) {
-            e.preventDefault();
-            (this.classList.contains("is-active") === true) ? this.classList.remove("is-active") : this.classList.add("is-active");
-        });
-    }
-})();
 
 window.addEventListener("resize", () => {
     if (window.innerWidth >= 768) {
